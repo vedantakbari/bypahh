d1a016c6385a33d9eed39d474818681800cf2596
vedantakbari <159980865+vedantakbari@users.noreply.github.com>
2025-07-09 09:31:32 -0700

    Create API.js

---
+++ API.js
@@ -0,0 +1,10 @@
+window.BaconButProAPI = "https://bacon-userscript.onrender.com/bypass?url=";
+window.BaconButProAPI2 = "https://userscript-api-vgv1.onrender.com/bypass?url=";
+window.BaconButProVersion = ""; // Version checking disabled
+window.BaconButProVersion2 = "https://bacon-userscript.onrender.com/Version";
+window.BaconButProKey = "Beacon";
+window.IwoozieAPI = "https://iwoozie.baby/api/free/bypass?url=";
+window.BaconButProUpdates = "https://check-whitelist.vercel.app/updates/userscript/";
+window.BaconButProUpdates2 = "https://bacon-userscript.onrender.com/updates/userscript/";
+window.BaconButProLogIP = "https://check-whitelist.vercel.app/logip?&apikey=";
+window.BaconButProLogIP2 = "https://bacon-userscript.onrender.com/logip?&apikey=";
